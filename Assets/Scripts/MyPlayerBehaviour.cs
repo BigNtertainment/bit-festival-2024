928973bc39113e72ecc3adfc33ca3f56534a7033
Bartosz Bieniek <itsbk20@gmail.com>
2024-12-01 04:10:15 +0100

    Update SampleScene to use CutBackToSceneRestarter

--- Assets/Scripts/MyPlayerBehaviour.cs
+++ Assets/Scripts/MyPlayerBehaviour.cs
@@ -3,38 +3,46 @@ using UnityEngine;
 
 public class MyPlayerBehaviour : MonoBehaviour
 {
-    private readonly DialogueLine _dialogueLine = new DialogueLine
-    {
-        CharacterName = "Executioner",
-        Text = "Arrggh. I'm gona execute him now.",
-        Answers = new DialogueAnswer[]
-        {
-            new()
-            {
-                Text = "[Continue]",
-                NextDialogueLine = new DialogueLine
-                {
-                    CharacterName = "Executioner",
-                    Text = "Oh no! What happened to the platform!?",
-                    Answers = new DialogueAnswer[]
-                    {
-                        new()
-                        {
-                            Text = "[Under your breath] There's hope",
-                        }
-                    },
-                }
-            },
-            new()
-            {
-                Text = "It's over!",
-                OnClickAction = () => { Debug.Log("I'm over!"); }
-            }
-        }
-    };
+    private DialogueLine _dialogueLine;
 
     void Start()
     {
+        _dialogueLine = new DialogueLine
+        {
+            CharacterName = "Executioner",
+            Text = "Arrggh. I'm gona execute him now.",
+            Answers = new DialogueAnswer[]
+            {
+                new()
+                {
+                    Text = "[Continue]",
+                    NextDialogueLine = new DialogueLine
+                    {
+                        CharacterName = "Executioner",
+                        Text = "Oh no! What happened to the platform!?",
+                        Answers = new DialogueAnswer[]
+                        {
+                            new()
+                            {
+                                Text = "[Under your breath] There's hope",
+                            }
+                        },
+                    }
+                },
+                new()
+                {
+                    Text = "It's over!",
+                    OnClickAction = () =>
+                    {
+                        Debug.Log("I'm over!");
+
+                        var cutToBlackSceneRestarter = FindFirstObjectByType<CutToBlackSceneRestarter>();
+                        cutToBlackSceneRestarter.CutToBlackAndRestartScene();
+                    }
+                }
+            }
+        };
+
         var dialogueTrigger = GetComponent<DialogueTrigger>();
         dialogueTrigger.TriggerDialogue(_dialogueLine);
     }
