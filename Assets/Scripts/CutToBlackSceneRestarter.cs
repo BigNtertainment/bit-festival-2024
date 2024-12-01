080c9b20ff14d95dee99f252a37bc2680548246c
Bartosz Bieniek <itsbk20@gmail.com>
2024-12-01 04:07:19 +0100

    Create CutToBlackSceneRestarter

---
+++ Assets/Scripts/CutToBlackSceneRestarter.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutToBlackSceneRestarter : MonoBehaviour
+{
+    public GameObject blackScreenPanel;
+
+    public void CutToBlackAndRestartScene()
+    {
+        blackScreenPanel.SetActive(true);
+
+        var waitForSecondsJob = WaitForSeconds(1f).Then(RestartScene);
+        StartCoroutine(waitForSecondsJob);
+    }
+
+    private IEnumerator WaitForSeconds(float waitSeconds)
+    {
+        var timeElapsed = 0f;
+
+        while (timeElapsed < waitSeconds)
+        {
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void RestartScene()
+    {
+        var activeSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSceneBuildIndex);
+    }
+}
+
+static class EnumeratorExtension
+{
+    public static IEnumerator Then(this IEnumerator currentEnumerator, Action followupAction)
+    {
+        while (currentEnumerator.MoveNext())
+        {
+            yield return currentEnumerator.Current;
+        }
+
+        followupAction();
+    }
+}
